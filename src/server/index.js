fae3eb58bcd52c3edc8dbb32d7bb904e65644075
Mayank Suman <mayank@simpplr.com>
2021-05-09 15:49:21 +0530

    refactor some code

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
 
-import App from './App';
+import App from './App.jsx';
 
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(<App />, document.getElementById('app'));
