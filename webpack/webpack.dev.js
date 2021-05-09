3f873340c99755d674443176681ae0e4999aacbe
Mayank Suman <mayank@simpplr.com>
2021-05-09 15:47:53 +0530

    add webpack configs

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,31 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules\//,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [require.resolve('react-refresh/babel')],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new ReactRefreshWebpackPlugin()],
+  devServer: {
+    contentBase: './public',
+    contentBasePublicPath: '/assets',
+    hot: true,
+  },
+});
