03fdbde9bce8a6200a015f1d05a6a4e62352ad14
Mayank Suman <mayank@simpplr.com>
2021-05-18 15:57:14 +0530

    add terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,30 @@
-// rollup.config.js
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+// import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
 import pkg from './package.json';
 
-const isProd = process.env.BUILD === 'production';
+const getUmdConfig = ({ minify } = { minify: false }) => ({
+  external: ['react'],
+  input: pkg.source,
+  output: {
+    name: pkg.name,
+    file: minify ? pkg.browser.replace('.js', '.min.js') : pkg.browser,
+    format: 'umd',
+    globals: {
+      react: 'React',
+    },
+  },
+  plugins: [resolve(), commonjs(), ...[minify && terser()]],
+});
 
 export default [
+  getUmdConfig(),
+  getUmdConfig({ minify: true }),
   {
-    input: 'src/index.js',
-    output: {
-      name: pkg.name,
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-    ],
-  },
-  {
-    input: 'src/index.js',
-    external: ['ms', 'react'],
+    external: ['react'],
+    input: pkg.source,
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
